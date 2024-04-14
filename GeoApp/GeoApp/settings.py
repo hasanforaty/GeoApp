a9a022dfabfc41f4270a0437ac1c7ad3c9224bb5
hasan foraty <hasanforaty19@gmail.com>
2024-04-14 22:15:19 +0330

    adding api

--- GeoApp/GeoApp/settings.py
+++ GeoApp/GeoApp/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'drf_spectacular',
-    'django.contrib.gis'
+    'django.contrib.gis',
+    'api'
 ]
 
 MIDDLEWARE = [
