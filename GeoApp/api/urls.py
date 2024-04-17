6a7669f37320db7780af97819ada546d55f24626
hasan foraty <hasanforaty19@gmail.com>
2024-04-17 12:59:25 +0330

    fix issue with url

--- GeoApp/api/urls.py
+++ GeoApp/api/urls.py
@@ -3,5 +3,5 @@ from api.views import ShapeFileUploadApiView, FeatureApiView
 
 urlpatterns = [
     path('shpefile/', ShapeFileUploadApiView.as_view()),
-    path('layer/(?P<layer_name>[^/.]+)/features/',FeatureApiView.as_view())
+    path('layer/<layer_name>/features/',FeatureApiView.as_view())
 ]
